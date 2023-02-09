4e1b39f6b22f2bc73d2da40694e41611118c2941
AddisonJPratt <pra21014@byui.edu>
2023-02-09 02:45:24 -0700

    ok

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import joblib
 import streamlit as st
+import numpy as np
 
-loaded_model = joblib.load("HeartSurvivalModel.sav")
+loaded_model = joblib.load("filename.joblib")
 
 
 st.title("Did they survive? :heart:")
+lst = np.arange(1, 101).tolist()
+st.select_slider("Select ejection fraction", lst)
